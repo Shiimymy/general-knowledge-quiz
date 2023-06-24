cbc23ed2b9f5fc08e2b40433556b4e3f40620a8a
Shiimymy <>
2023-06-24 17:21:17 +0000

    Add js main structure

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () { });
+
+function getAnswers() { };
+
+function checkAnswers() { };
+
+function incrementScore() { };
+
+function displayImage() { };
+
+function displayFeedback() { };
